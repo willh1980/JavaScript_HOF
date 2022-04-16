78fac813e60d139a06cb196b29ffd4d19aba7bdb
Will Hurst <will.hurstjr@gmail.com>
2022-04-15 17:57:13 -0700

    final

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plus(number) {
+
+    return function (plusnumber =0){
+        return number + plusnumber;
+    }
+}
+
+const plus15 = plus(15);
+
+console.log(plus15(10))
